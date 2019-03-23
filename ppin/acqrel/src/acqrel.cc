a4fffd369ee638147612b11a7525026ef6dd82f7
mahesh <mahesh@paraqum.com>
2019-03-23 12:25:45 +0000

    add mfence detectable pin program

--- ppin/acqrel/src/acqrel.cc
+++ ppin/acqrel/src/acqrel.cc
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "pin.H"
+#include <iostream>
 
 enum MemType
 {
@@ -46,7 +47,21 @@ void PIN_FAST_ANALYSIS_CALL execNonMem(uint32_t ins_count, THREADID threadid)
 void execMem(void * addr, THREADID threadid, uint32_t size, bool mem_type)
 {
    // core_manager->execMem(addr, threadid, size, mem_type);
-    if(threadid > 0) printf("Mem Thread Id : %d %d %s %d \n", threadid, *((int *)addr), (mem_type)?"RD":"WR", size);
+    //if(threadid > 0) printf("Mem Thread Id : %d %d %s %d \n", threadid, *((int *)addr), (mem_type)?"RD":"WR", size);
+}
+
+void memIns(void * addr, THREADID threadid, uint32_t size, bool mem_type, void *ip, void *ins, uint32_t next_op, bool valid)
+{
+   // core_manager->execMem(addr, threadid, size, mem_type);
+   if(valid) {
+        if(next_op == 0x17a){
+            std::cout << next_op << " \n" << std::endl;
+        }
+    
+   }
+    //std::cout << INS_Mnemonic(*in) << std::endl;    
+
+    //if(threadid > 0) printf("Mem Thread Id : %d %d %s %d %s \n", threadid, *((int *)addr), (mem_type)?"RD":"WR", size, INS_Mnemonic(*in).c_str());
 }
 
 // Pin calls this function every time a new basic block is encountered
@@ -61,56 +76,81 @@ void Trace(TRACE trace, void *v)
         BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)insCount, IARG_FAST_ANALYSIS_CALL, 
                        IARG_UINT32, BBL_NumIns(bbl), IARG_THREAD_ID, IARG_END);
 
-        //BBL_NumIns (how arguments work)
-        //https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__BBL__BASIC__API.html#ga76ceb2d9e0fb974d3c3769b2413ed634
-
         nonmem_count = 0;
         INS ins = BBL_InsHead(bbl);
+        //INS ins_next =BBL_InsHead(bbl);
+        //BBL lib - https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__BBL__BASIC__API.html
 
-        //Ins Object : https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__INS__BASIC__API.html
-        
-        for (; INS_Valid(ins); ins = INS_Next(ins)) {
+        //for (; INS_Valid(ins); ins = INS_Next(ins)) {
+        for (; INS_Valid(ins); ins = INS_Next(ins)) {           
+            
 
-            // Instruments memory accesses using a predicated call, i.e.
-            // the instrumentation is called iff the instruction will actually be executed.
-            //
-            // The IA-64 architecture has explicitly predicated instructions. 
-            // On the IA-32 and Intel(R) 64 architectures conditional moves and REP 
-            // prefixed instructions appear as predicated instructions in Pin.
-            if(INS_IsMemoryRead(ins) || INS_IsMemoryWrite(ins)) { 
+            //OPCODE opcode = INS_Opcode(ins); //Unused
+            //printf("OPCODE - %x \n", (int) opcode);    
+            //std::cout <<  INS_Mnemonic(ins) << std::endl;        
+
+            if(INS_IsMemoryRead(ins) || INS_IsMemoryWrite(ins)) {
+
                 uint32_t memOperands = INS_MemoryOperandCount(ins);
+                //uint32_t next_addr = INS_NextAddress (ins); //Unused
+                INS ins_next = INS_Next(ins);
+                //OPCODE opcode_next = INS_Opcode(ins_next);
+                bool isvalid = INS_Valid(ins_next);
+                uint32_t opn = 0;
+                if(isvalid) {
+                     opn = (uint32_t)INS_Opcode(ins_next);
+                }                 
 
                 // Iterate over each memory operand of the instruction.
-                for (uint32_t memOp = 0; memOp < memOperands; memOp++) {
+                for (uint32_t memOp = 0; memOp < memOperands; memOp++){
+
                     const uint32_t size = INS_MemoryOperandSize(ins, memOp);
+
                     // Note that in some architectures a single memory operand can be 
                     // both read and written (for instance incl (%eax) on IA-32)
                     // In that case we instrument it once for read and once for write.
-                    if (INS_MemoryOperandIsRead(ins, memOp)) {
+
+                    if (INS_MemoryOperandIsRead(ins, memOp))
+                    {
                         INS_InsertPredicatedCall(
-                            ins, IPOINT_BEFORE, (AFUNPTR)execMem,
+                            ins, IPOINT_BEFORE, (AFUNPTR)memIns,
                             IARG_MEMORYOP_EA, memOp,
                             IARG_THREAD_ID,
                             IARG_UINT32, size,
                             IARG_BOOL, RD,
+                            IARG_INST_PTR,
+                            IARG_PTR, ins,
+                            IARG_UINT32, opn, 
+                            IARG_BOOL, isvalid,
                             IARG_END);
 
                     }
                     if (INS_MemoryOperandIsWritten(ins, memOp)) {
                         INS_InsertPredicatedCall(
-                            ins, IPOINT_BEFORE, (AFUNPTR)execMem,
+                            ins, IPOINT_BEFORE, (AFUNPTR)memIns,
                             IARG_MEMORYOP_EA, memOp,
                             IARG_THREAD_ID,
                             IARG_UINT32, size,
                             IARG_BOOL, WR,
+                            IARG_INST_PTR,
+                            IARG_PTR, ins,
+                            IARG_UINT32, opn, 
+                            IARG_BOOL, isvalid,
                             IARG_END);
                     }
                 }
+            
             }
+            
             else {
-                nonmem_count++;
+                    nonmem_count++;
             }
-        }
+
+                    //ins=ins_next;
+
+        } //END INS LOOP
+            
+        
         // Insert a call to execNonMem before every bbl, passing the number of nonmem instructions
         BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)execNonMem, IARG_FAST_ANALYSIS_CALL, 
                        IARG_UINT32, nonmem_count, IARG_THREAD_ID, IARG_END);
@@ -119,7 +159,78 @@ void Trace(TRACE trace, void *v)
 
 }
 
+VOID Fini(INT32 code, VOID *v)
+{
+    fprintf(trace, "#eof\n");
+    fclose(trace);
+}
 
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+   
+INT32 Usage()
+{
+    PIN_ERROR( "This Pintool prints a trace of memory addresses\n" 
+              + KNOB_BASE::StringKnobSummary() + "\n");
+    return -1;
+}
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
+
+int main(int argc, char *argv[])
+{
+    if (PIN_Init(argc, argv)) return Usage();
+
+    trace = fopen("acqrel.out", "w");
+
+    //INS_AddInstrumentFunction(Instruction, 0);
+    TRACE_AddInstrumentFunction(Trace, 0);
+
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Never returns
+    PIN_StartProgram();
+    
+    return 0;
+}
+
+        //Intel BIB
+        ////BBL lib - https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__BBL__BASIC__API.html
+
+        //All INS parameters
+        //Ins Object : https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__INS__BASIC__API.html
+        // Memory Barriers http://www.modernescpp.com/index.php/fences-as-memory-barriers
+
+        //BBL_NumIns (how arguments work)
+        //https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__BBL__BASIC__API.html#ga76ceb2d9e0fb974d3c3769b2413ed634
+
+        //BBL Cache line flush
+        //https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__INS__BASIC__API.html
+        
+
+        //Ins_..()
+        //INS API : https://software.intel.com/sites/landingpage/pintool/docs/81205/Pin/html/group__INS__BASIC__API__GEN__IA32.html
+
+        //ALl arguments - parameters
+        //https://software.intel.com/sites/landingpage/pintool/docs/97619/Pin/html/group__INST__ARGS.html#ga089c27ca15e9ff139dd3a3f8a6f8451d
+
+        //Who ordered an MFENCE in x86
+        //https://bartoszmilewski.com/2008/11/05/who-ordered-memory-fences-on-an-x86/
+
+              //printf("MemOp\n");
+                //if(INS_Mnemonic(INS_Next(ins)) == "MFENCE") printf("MFENCE \n"); //https://www.felixcloutier.com/x86/mfence
+
+                //if(INS_IsAtomicUpdate(ins)) std::cout << INS_Mnemonic(ins) << std::endl;
+                //ALl INS Function
+                //https://software.intel.com/sites/landingpage/pintool/docs/81205/Pin/html/group__INS__BASIC__API__GEN__IA32.html
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Functions
 /*
 // Is called for every instruction and instruments reads and writes
 VOID Instruction(INS ins, VOID *v)
@@ -158,40 +269,3 @@ VOID Instruction(INS ins, VOID *v)
     }
 }
 */
-VOID Fini(INT32 code, VOID *v)
-{
-    fprintf(trace, "#eof\n");
-    fclose(trace);
-}
-
-/* ===================================================================== */
-/* Print Help Message                                                    */
-/* ===================================================================== */
-   
-INT32 Usage()
-{
-    PIN_ERROR( "This Pintool prints a trace of memory addresses\n" 
-              + KNOB_BASE::StringKnobSummary() + "\n");
-    return -1;
-}
-
-/* ===================================================================== */
-/* Main                                                                  */
-/* ===================================================================== */
-
-int main(int argc, char *argv[])
-{
-    if (PIN_Init(argc, argv)) return Usage();
-
-    trace = fopen("pinatrace.out", "w");
-
-    //INS_AddInstrumentFunction(Instruction, 0);
-    TRACE_AddInstrumentFunction(Trace, 0);
-
-    PIN_AddFiniFunction(Fini, 0);
-
-    // Never returns
-    PIN_StartProgram();
-    
-    return 0;
-}
