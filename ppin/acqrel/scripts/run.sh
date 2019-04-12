f57fcb1b0044b847d6cbfe1d96b1c6178d29c908
Mahesh Wickrama Arachchilage <s1797403@james12.inf.ed.ac.uk>
2019-04-12 11:13:34 +0100

    update run past scripts

--- ppin/acqrel/scripts/run.sh
+++ ppin/acqrel/scripts/run.sh
@@ -1,7 +1,1 @@
-#!/bin/bash  
-program_name=acqrel
-
-echo 'Starting Pin..'
-echo $program_name
-source ../../../env.sh
-pin.sh -ifeellucky -t /home/mahesh/mahesh/edinburgh/repos/primesim/ppin/acqrel/bin/acqrel.so -- /bin/ls
+pin.sh -ifeellucky -t /afs/inf.ed.ac.uk/user/s17/s1797403/repos/primesim/ppin/acqrel/bin/acqrel.so -- /bin/ls
