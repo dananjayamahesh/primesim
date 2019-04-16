5df1f169e69fbdb5890f38af1bb86586483bdb8e
Mahesh Wickrama Arachchilage <s1797403@albert.inf.ed.ac.uk>
2019-04-16 13:27:06 +0100

    fixed adding thread_0 cycle twice

--- src/core_manager.cpp
+++ src/core_manager.cpp
@@ -418,14 +418,16 @@ void CoreManager::report(ofstream *result)
     uint64_t total_cycles = 0, total_ins_counts = 0,  total_nonmem_ins_counts = 0, dimp_cycles=0;
     double total_cycles_nonmem = 0;
     total_cycles = (uint64_t)(cycle[0]._count);
-    dimp_cycles = (uint64_t)(cycle[0]._count);
+    //dimp_cycles = (uint64_t)(cycle[0]._count);
+    dimp_cycles = 0;
+    uint64_t thread_0_cycles =0;
     int i;
     for (i = 0; i < max_threads; i++) {
         total_ins_counts += ins_count[i]._count;
         total_nonmem_ins_counts += (uint64_t)ins_nonmem[i]._count;
         dimp_cycles += (uint64_t)(cycle[i]._count);
         //cout << "[PriME] Thread " <<i<< " runs " << (uint64_t)ins_count[i]._count <<" instructions\n";
-        cout << "[PriME] Thread " <<i<< " runs " << (uint64_t)ins_count[i]._count <<" instructions, Non-mem Ins"<< (uint64_t)ins_nonmem[i]._count << "So Mem Ins : "<< (uint64_t)(ins_count[i]._count -  ins_nonmem[i]._count )<<"\n";
+        cout << "[PriME] Thread " <<i<< " runs " << (uint64_t)ins_count[i]._count <<" instructions, Non-mem Ins - "<< (uint64_t)ins_nonmem[i]._count << "So Mem Ins - "<< (uint64_t)(ins_count[i]._count -  ins_nonmem[i]._count )<<"\n";
     }
     total_cycles_nonmem = total_nonmem_ins_counts * cpi_nonmem;
     sim_time = (sim_finish_time.tv_sec - sim_start_time.tv_sec) + (double) (sim_finish_time.tv_nsec - sim_start_time.tv_nsec) / 1000000000.0; 
