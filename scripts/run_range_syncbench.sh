a826a80d934783520de2503cf49673b53d27820f
Mahesh Wickrama Arachchilage <s1797403@james12.inf.ed.ac.uk>
2019-04-22 16:40:27 +0100

    update run scripts

--- scripts/run_range_syncbench.sh
+++ scripts/run_range_syncbench.sh
@@ -20,19 +20,19 @@ do
 	for rate in 1000
 	do
 		#for rate2 in 200 400 600 800 1000 1200 1400 1600 1800 2000 10000
-		for rate2 in 4000 16000 32000 64000
+		for rate2 in 4000 8000 16000 32000
 		do
 			
 			if [ $rate2 -lt $rate ] ; then 
 				continue 
 			fi
 
-			operations=2000
+			#operations=2000
 			
-			for bench in linkedlist
+			for bench in linkedlist hashmap bstree skiplist-rotating-c
 			do	
-				echo "$bench,$threads,$rate,$rate2" >> ${DATA_FILE}
-				for pmodel in 3 4 0
+				echo "$bench,$threads,$rate,$rate2,$operations" >> ${DATA_FILE}
+				for pmodel in 0 3 4
 				do
 					echo "Executing $pmodel"
 					#operations=${rate2}
@@ -42,22 +42,22 @@ do
 					mpiexec --verbose --display-map --display-allocation -mca btl_sm_use_knem 0 \
 					-np 1 ${HOME}/repos/primesim/bin/prime \
 					${HOME}/repos/primesim/xml/config_${pmodel}.xml \
-					${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out : \
+					${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out : \
 					-np 1 pin.sh -ifeellucky -t ${HOME}/repos/primesim/bin/prime.so \
 					-c ${HOME}/repos/primesim/xml/config_${pmodel}.xml \
-					-o ${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out \
+					-o ${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out \
 					-- ${HOME}/repos/primesim/pbench/syncbench/bin/${bench} -t ${threads} -i ${rate} -r ${rate2} -o ${operations} -d 10 -x 6 -u 50
 					
 					#cat ${DIMP_FILE} >> ${DATA_FILE3}
 					cat ${DIMP_FILE} >> ${DATA_FILE}
 					
-					cat ${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1 \
-					${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0 \
-					> ${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out
+					cat ${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1 \
+					${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0 \
+					> ${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out
 										
-					rm -f  ${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0
+					rm -f  ${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0
 					
-					rm -f  ${HOME}/repos/primesim/output/${CONF_NAME}/config_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1
+					rm -f  ${HOME}/repos/primesim/output/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1
 				done
 			   
 			done
