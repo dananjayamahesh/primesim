b0a7b9f0756e20c78b401331c4e2033d23367a4d
Mahesh Wickrama Arachchilage <s1797403@celeste.inf.ed.ac.uk>
2019-11-15 16:53:44 +0000

    added acripts for pbuff tetss

---
+++ scripts/balanced/run_mbank_120cycles_RET.sh
@@ -0,0 +1,120 @@
+#I am fixing few bugs. 
+# 1. NOP does not need any write backs.
+#2 rp replacement
+#3 core id delay
+#4 remove release persistency  replacement writeback.
+#remove delay from natural write back. it may have effected core 1
+
+#balanced, addony, mremoved
+optype=balanced
+#RUn History
+#syncbench-mbank-120cycles-nopfixed-4 ->initial run after fixing rp writeback delays
+#syncbench-mbank-120cycles-nopfixed-4-1 ->same
+#syncbench-mbank-120cycles-nopfixed-4-2 ->add more counters for write backs and epoch sizes, add thread 1,
+#CONF_NAME=syncbench-mbank-120cycles-nopfixed-6 original with u 100
+
+##Did not run for 25
+#44-3 has stat print fixed with shares and invals. als 44-4 (regular one has partially)
+
+#42-5 and 42-6 has full stat including invals
+
+#RET Evaluation only for the hashmap-8 threads and 4000 operations.
+#03 - with stats 1000 operations.
+#04 - 4000
+#05 - res cycle 2000
+#06 - res cycle 4000
+#7 1000
+#8 10000
+
+#Run for the queue
+#9 lfqueue 4000
+#10 lfqueue2 4000
+#11 lfqueue2 10000
+#12 lfqueue 10000
+CONF_NAME=syncbench-mbank-120cycles-nopfixed-RET-12
+CONF_PATH=${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/
+DIMP_FILE=${HOME}/repos/primesim/output/${optype}/stat.txt
+DIMP2_FILE=${HOME}/repos/primesim/output/${optype}/stat2.txt
+DATA_FILE=${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/data.txt
+DATA2_FILE=${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/data2.txt
+
+
+if [ ! -d ${CONF_PATH} ] 
+then
+    mkdir -p ${CONF_PATH}
+fi 
+
+#echo "" > ${DIMP_FILE}
+#> ${DIMP_FILE}
+> ${DATA_FILE}
+> ${DATA2_FILE}
+
+#make -B
+#SyncMap is 10000
+
+#for upd in 50 100
+#do
+#make -B
+
+urate=100
+rate=1000 #original rate = 1000
+rate2=64000
+for rate in 1000 
+do
+#for threads in 8 16 32
+	for operations in 10000
+	do
+		#for operations in 1000 4000
+		#for threads in 1 8 16 32
+		for threads in 32
+		do
+				
+				#for bench in hashmap
+				for bench in lfqueue
+				#for bench in linkedlist hashmap bstree skiplist lfqueue lfqueue2
+				#for bench in linkedlist hashmap bstree skiplist lfqueue lfqueue2 locklist
+				#for bench in bstree skiplist-rotating-c
+				do	
+					echo "$bench,$threads,$rate,$rate2,$operations" >> ${DATA_FILE}
+					echo "$bench,$threads,$rate,$rate2,$operations" >> ${DATA2_FILE}
+						            	
+					for pmodel in 3
+					do
+						echo "Executing $pmodel"
+						#operations=${rate2}
+						#echo "$bench,$pmodel," >> ${DIMP_FILE}
+						#echo "$pmodel" >> ${DATA_FILE}
+						
+						mpiexec --verbose --display-map --display-allocation -mca btl_sm_use_knem 0 \
+						-np 1 ${HOME}/repos/primesim/bin/prime \
+						${HOME}/repos/primesim/xml/shared-llc/config_mbank_120cycles_${pmodel}.xml \
+						${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out : \
+						-np 1 pin.sh -ifeellucky -t ${HOME}/repos/primesim/bin/prime.so \
+						-c ${HOME}/repos/primesim/xml/shared-llc/config_mbank_120cycles_${pmodel}.xml \
+						-o ${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out \
+						-- ${HOME}/repos/primesim/pbench/syncbench/bin/${bench}_${optype} -t ${threads} -i ${rate} -r ${rate2} -o ${operations} -d 10 -x 6 -u ${urate}
+						
+						#cat ${DIMP_FILE} >> ${DATA_FILE3}
+						DIMP_FILE=${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_stat
+						DIMP2_FILE=${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_stat2
+	
+						cat ${DIMP_FILE} >> ${DATA_FILE}
+						#cat ${DIMP2_FILE} >> ${DATA2_FILE}
+						cat ${DIMP2_FILE} | head -n 1 >> ${DATA2_FILE}
+						
+						cat ${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1 \
+						${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0 \
+						> ${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out
+											
+						rm -f  ${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_0
+						
+						rm -f  ${HOME}/repos/primesim/output/${optype}/${CONF_NAME}/config_${threads}_${rate}_${rate2}_${operations}_${bench}_${pmodel}.out_1
+						rm -f ${DIMP_FILE}
+						rm -f ${DIMP2_FILE}
+					done
+				   
+				done
+			
+		done
+	done
+done
